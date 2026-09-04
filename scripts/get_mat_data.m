d21c62fc233eef00824eccb7ff71146490c7e3d6
Emptea <aryas1725@gmail.com>
2026-09-04 16:59:15 +0300

    added tp 6-8 to mat data get

--- scripts/get_mat_data.m
+++ scripts/get_mat_data.m
@@ -1,4 +1,9 @@
-function sg = get_mat_data(folder, tp_num)
+function sg = get_mat_data(folder, tp_num, range_gate)
+arguments
+    folder string
+    tp_num double
+    range_gate double = 0
+end
 switch(tp_num)
     case {1,2}
         load(folder + "/sig_tp1_fd.mat")
@@ -14,4 +19,29 @@ switch(tp_num)
         load(folder + "/sig_tp4_sf_near.mat")
         sg = tp4_sf_far;
         sg(:,102:141, :) = tp4_sf_near;
+    case 6
+        load(folder + "/sig_tp5_ddr_far.mat")
+        load(folder + "/sig_tp5_ddr_near.mat")
+        if (range_gate < 101)
+            sg = permute(tp5_ddr_far(:, range_gate + 1, :, :), [1 3 4 2]);
+        else
+            sg = permute(tp5_ddr_near(:, range_gate + 1 - 101, :, :), [1 3 4 2]);
+        end
+    case 7
+        load(folder + "/sig_tp6_kn_far.mat")
+        
+        if range_gate < 101
+            load(folder + "/sig_tp6_kn_far.mat")
+            sg = permute(tp6_kn_far(:, range_gate + 1, :, :), [1 3 4 2]);
+        else
+            load(folder + "/sig_tp6_kn_near.mat")
+            sg = permute(tp6_kn_near(:, range_gate + 1 - 101, :, :), [1 3 4 2]);
+        end
+    case 8
+        load(folder + "/sig_tp7_ad_far.mat")
+        load(folder + "/sig_tp7_ad_near.mat")
+        sg = tp7_ad_far;
+        sg(102:141, :) = tp7_ad_near;
+    case 9
+        
 end
