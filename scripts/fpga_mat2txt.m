6b357e268aaa0a266d85e1a651a18724e20a4cd5
Emptea <aryas1725@gmail.com>
2026-09-15 20:37:56 +0300

    mat2txt without scaling

--- scripts/fpga_mat2txt.m
+++ scripts/fpga_mat2txt.m
@@ -8,31 +8,31 @@ out_filename = folder + "/" + name + ".txt";
 load(filename);
 % Определяем тип файла
 if contains(file, "tp1-fd") || contains(file, "tp1_fd")
-    d = fpga_double2fxp(tp1_fd);    
+    % d = fpga_double2fxp(tp1_fd);    
+    d = tp1_fd;    
 elseif contains(file, "tp2-fap") || contains(file, "tp2_fap")
-    d = fpga_double2fxp(tp2_fap);
+    % d = fpga_double2fxp(tp2_fap);
+    d = tp2_fap;
 elseif contains(file, "tp3-lou") || contains(file, "tp3_lou")
-    d = fpga_double2fxp(tp3_lou);
+    % d = fpga_double2fxp(tp3_lou);
+    d = tp3_lou;
 elseif contains(file, "tp4-sf-far") || contains(file, "tp4_sf_far")
-    n_sf = 40;
-    d = fpga_double2fxp(tp4_sf_far / n_sf);
+    % d = fpga_double2fxp(tp4_sf_far);
+    d = tp4_sf_far;
 elseif contains(file, "tp4-sf-near") || contains(file, "tp4_sf_near")
-    n_sf = 6;
-    d = fpga_double2fxp(tp4_sf_near / n_sf);
+    % d = fpga_double2fxp(tp4_sf_near);
+    d = tp4_sf_near;
 elseif contains(file, "tp4-sf-both") || contains(file, "tp4_sf_both")
-    n_sf_far = 40;
-    tp4_sf_both(:,1:101,:) = tp4_sf_both(:,1:101,:) ./ n_sf_far;
-    n_sf_near = 6;
-    tp4_sf_both(:,102:end,:) = tp4_sf_both(:,102:end,:) ./ n_sf_near;
-    d = fpga_double2fxp(tp4_sf_both / n_sf_near);
+    tp4_sf_both(:,1:101,:) = tp4_sf_both(:,1:101,:);
+    tp4_sf_both(:,102:end,:) = tp4_sf_both(:,102:end,:);
+    % d = fpga_double2fxp(tp4_sf_both_near);
+    d = tp4_sf_both
 elseif contains(file, "tp5-kn-far") || contains(file, "tp5_kn_far")
-    n_sf = 40;
-    n_fft = 512;
-    d = fpga_double2fxp(tp5_kn_far / n_sf / n_fft);
+    % d = fpga_double2fxp(tp5_kn_far);
+    d = tp5_kn_far;
 elseif contains(file, "tp5-kn-near") || contains(file, "tp5_kn_near")
-    n_sf = 6;
-    n_fft = 512;
-    d = fpga_double2fxp(tp5_kn_near / n_sf / n_fft);
+    % d = fpga_double2fxp(tp5_kn_near);
+    d = tp5_kn_near;
 else
     return;
 end
