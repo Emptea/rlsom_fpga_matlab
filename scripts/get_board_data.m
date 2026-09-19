7cd3c46f965a159ea5bd3976da39d06aa62e2f4a
Emptea <aryas1725@gmail.com>
2026-09-19 17:19:36 +0300

    fix get board data

--- scripts/get_board_data.m
+++ scripts/get_board_data.m
@@ -6,22 +6,22 @@ arguments
 end
 
 prog_path = "~/drivers/ip_comm_test/build/";
-remote_folder = prog_path + date + "/";
+board_files = fullfile(prog_path, date, basename + "*");
 if ~exist(savefolder, 'dir')
     mkdir(savefolder)
 end
 
-system_cmd = ...
-    "rsync -av --include='" + basename + "*' --exclude='*' " + ...
-    "mini:" + remote_folder + " """ + savefolder + "/""";
-[status, cmdout] = system(system_cmd);
+system_cmd = "scp ""mini:" + board_files + """ """ + savefolder + "/""";
 
+[status, cmdout] = system(system_cmd + " 2>&1");
+
+save_files = fullfile(savefolder, basename + "*");
 if status == 0
     fprintf('✓ Files copied successfully from %s%s/%s/ to %s\n', ...
         prog_path, date, basename, savefolder);
     % List copied files
-    ls(savefolder + basename + "*");
+    fprintf('%s\n', dir(save_files).name);
 else
-    fprintf('✗ rsync failed with status: %d\n', status);
+    fprintf('✗ scp failed with status: %d\n', status);
     disp(cmdout);
 end
